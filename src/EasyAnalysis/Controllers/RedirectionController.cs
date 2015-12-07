b60908408cc4e5b0ddc529aa54158c8b3042e0fe
dream-365 <support-jec@hotmail.com>
2015-12-07 16:58:00 +0800

    external tool intergration - url redirection support

--- src/EasyAnalysis/Controllers/RedirectionController.cs
+++ src/EasyAnalysis/Controllers/RedirectionController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Dapper;
 
 namespace EasyAnalysis.Controllers
 {
@@ -24,5 +27,41 @@ namespace EasyAnalysis.Controllers
 
             return Redirect(url);
         }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">guid | number</param>
+        /// <param name="exteranl">tool name</param>
+        /// <returns></returns>
+        public ActionResult Navigate(string id, string external)
+        {
+            if (external == "sotool")
+            {
+                var identifier = string.Format("SO_{0}", id);
+
+                string cs = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+                using (var connection = new SqlConnection(cs))
+                {
+                    var repository = connection.Query("SELECT [Repository] As [Name] FROM [dbo].[VwThreads] WHERE[Id] = @Id", new { Id = identifier })
+                                               .FirstOrDefault();
+
+                    // If the instance is not found in the system, return to home page
+                    if(repository == null)
+                    {
+                        return Redirect("/#/");
+                    }
+
+                    var url = UrlHelper.GenerateContentUrl(string.Format("~/#/detail/{0}/{1}", repository.Name, identifier), HttpContext);
+
+                    return Redirect(url);
+                }
+            }
+
+            return Redirect("/#/");
+        }
+
     }
 }
